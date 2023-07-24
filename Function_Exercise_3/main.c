7e96dda20415fbfab785659014d32759063dd0ab
Alejandro Reyes Leon <alle262001@gmail.com>
2023-07-24 11:13:55 -0500

    Function exercise 3.

--- Function_Exercise_3/main.c
+++ Function_Exercise_3/main.c
@@ -1,9 +1,44 @@
+/*
+    Project Description:
+    This is a exercise for calculate the square of entered number
+    however, without using "pow()" function.
 
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
+//Prototype function.
+long Square ();
+
+//Main function.
 int main()
 {
-    printf("Hello world!\n");
+    printf("Hello everyone and welcome to this program!\n");
+    printf("\n\aThe result is: %ld",Square());
+
     return 0;
 }
+
+//Develop function of square
+long Square (void){
+
+    //Local variables.
+    int Base = 0;
+    int Exponent = 0;
+    long Result = 1;
+
+    printf("\nEnter any number to calculate the power: ");
+    scanf("%d", &Base);
+    printf("\nEnter the exponent of the number: ");
+    scanf("%d", &Exponent);
+
+    for(int i = 1; i <= Exponent; i++){
+
+        Result *= Base;
+
+    }
+
+    return Result;
+
+}
+

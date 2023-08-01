c1d32bc314c3a09ec31bfb121f1b7508e1326889
Alejandro Reyes Leon <alle262001@gmail.com>
2023-07-31 22:03:07 -0500

    Create another pointer exercise from the class.

--- Pointers_Exercise_2/main.c
+++ Pointers_Exercise_2/main.c
@@ -12,16 +12,23 @@ int main()
 {
     //Create variables and the pointer.
     short a = 8, b = 10, *p;
-    p = &a;
-    printf("The pointer value is: %d", *p);
 
-    b = *p + 1;
+    p = &a; //Assigning to the pointer the A variable.
 
-    printf("\nThe value of the b variable is: %d", b);
+    printf("The pointer value is: %d", *p); //Show pointer.
 
-    *p = 26;
+    b = *p + 1; //Modifying the b variable.
 
-    printf("\nThe new value of pointer is: %d", *p);
+    printf("\nThe value of the b variable is: %d", b); //Show the variable.
+
+    *p = 26; //Assigning the to the pointer new value.
+
+    /*
+        Here I'll show the "a" variable because the address
+        of the pointer it's the same as that of the variable.
+        It's an indirect way to manipulating variable value.
+    */
+    printf("\nThe new value of pointer is: %d", a);
 
     return 0;
 }

d8dbebfe2b98c9e63cc926be868bd19f44be3abe
Daniel ALejandro Reyes León <alle262001@gmail.com>
2023-08-15 17:08:41 -0500

    First part of the project.

--- Struct_Exercise_2/main.c
+++ Struct_Exercise_2/main.c
@@ -23,9 +23,21 @@ struct Conservatory {
 //Main function.
 int main()
 {
+
     printf("Hello everyone and welcome to this program!\n\n");
+
+    //Create instance of the struct
+    struct Conservatory Student1;
+    Require_Data();
 
     return 0;
 }
 
 //Develop function to require to user data.
+void Require_Data (){
+
+    printf("\nEnter the personal information of the student: ");
+    printf("\nID: ");
+
+
+}

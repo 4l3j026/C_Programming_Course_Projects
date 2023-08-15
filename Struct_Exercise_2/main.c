4bd9069f0665093045c86d02272b43f610652e3e
Daniel ALejandro Reyes León <alle262001@gmail.com>
2023-08-15 16:13:35 -0500

    First part of the exercise.

---
+++ Struct_Exercise_2/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Project Description:
+        Calculate the average of the two students enter
+        his personal info.
+*/
+
+//Main function.
+int main()
+{
+    printf("Hello everyone and welcome to this program!\n\n");
+
+    return 0;
+}

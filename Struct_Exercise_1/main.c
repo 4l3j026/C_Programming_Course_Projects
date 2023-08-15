ddd8d78b6a848e2b7642ecb6297a8611f4a751ee
Daniel ALejandro Reyes León <alle262001@gmail.com>
2023-08-15 15:12:43 -0500

    Update the project showing in console the data string of the cello.

--- Struct_Exercise_1/main.c
+++ Struct_Exercise_1/main.c
@@ -7,22 +7,29 @@
 
 */
 
-//Struct
+//Struct function.
 struct Music{
 
+    //Create general variables.
     char Highs[10];
     char Mids[10];
     char Lows[10];
 
 };
+//Way to save values inside of the variables.
 struct Music Cello = {"A note.", "D & G notes.", "C note."};
 struct Music Violin = {"E note.", "A & D notes.", "G note."};
 
 //Main function.
 int main()
 {
+    //Show trough console the Cello notes.
     printf("Hello everyone and welcome to this program!\n");
-    printf("%s ; %s", Cello.Highs, Violin.Mids);
+    printf("The open strings of the cello are: \n");
+    printf("\t - The most highest string is: %s \n", Cello.Highs);
+    printf("\t - The middle strings are: %s \n", Cello.Mids);
+    printf("\t - The most lowest string is: %s \n", Cello.Lows);
+
     return 0;
 }
 

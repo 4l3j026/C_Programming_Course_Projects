e62029ec1d67f87aed9c6bbbbb8232c7c132c598
Daniel ALejandro Reyes León <alle262001@gmail.com>
2023-08-15 23:07:29 -0500

    Pending another part of the project.

--- Struct_Exercise_2/main.c
+++ Struct_Exercise_2/main.c
@@ -8,7 +8,7 @@
 */
 
 //Prototype Functions.
-void Require_Data (void);
+void Require_Data ();
 
 //Develop the struct function.
 struct Conservatory {
@@ -18,16 +18,19 @@ struct Conservatory {
         char Instrument[16];
         float Grades[5];
 
-};
+}Student1; //First way.
+
+struct Conservatory Student2; //Second way.
+
+//There are two ways to create structure variable like first or the second way.
 
 //Main function.
 int main()
 {
 
-    printf("Hello everyone and welcome to this program!\n\n");
+    printf("Hello everyone and welcome to this program!\n");
 
     //Create instance of the struct
-    struct Conservatory Student1;
     Require_Data();
 
     return 0;
@@ -36,8 +39,16 @@ int main()
 //Develop function to require to user data.
 void Require_Data (){
 
-    printf("\nEnter the personal information of the student: ");
+    printf("\nEnter the personal information of the student: \n");
+    printf("\nName: ");
+    gets(Student1.Name);
     printf("\nID: ");
-
+    scanf("%d", &Student1.ID);
+    printf("\nInstrument: ");
+    scanf("%s", &Student1.Name);
+    for (int i = 0; i < 5; i++){
+        printf("\nEnter note %d :", i+1);
+        scanf("%f", &Student1.Grades[i]);
+    }
 
 }

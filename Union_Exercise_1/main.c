78307b0da72ac2f77e8d34b0171439bf3944212e
Daniel ALejandro Reyes León <alle262001@gmail.com>
2023-09-02 21:29:53 -0500

    Create exercise of the union structure file.

---
+++ Union_Exercise_1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Project Description:
+        Through "union" structure, show address and data of the
+        some variables.
+
+*/
+
+int main()
+{
+    printf("Hi everyone and welcome to this program!\n");
+    return 0;
+}

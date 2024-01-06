7bfe65316d8137b05a7fb777726371621e7b0d07
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-01-06 13:11:18 -0500

    Create a project description.

--- Enum_Exercise_2/main.c
+++ Enum_Exercise_2/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+     Project Description: What is the order of the D minor scale?
+     Design a program to find the order of the notes, so the user
+     enter a random integer number, the program show the order of the note.
+*/
+
+//Structure
+enum Notes{
+
+    D = 1,
+    E,
+    F,
+    G,
+    A,
+    Bb,
+    C
+};
 int main()
 {
     printf("Hello world!\n");

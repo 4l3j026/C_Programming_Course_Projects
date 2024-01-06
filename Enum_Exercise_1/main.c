ca0e45606d6f592f57eca264e738fe45bb4c5271
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-01-05 22:36:38 -0500

    Update files of the project and finish enum statement for dynamic code.

--- Enum_Exercise_1/main.c
+++ Enum_Exercise_1/main.c
@@ -7,17 +7,46 @@
         some light of traffic.
 */
 
-int main()
-{
-    printf("Hello everyone and welcome to this program!\n");
+//Global variables
+unsigned char User_Number = 0;
 
-    return 0;
-}
+enum {
 
-enum Leds_Sequence{
-
+    Brown,
     Green,
     Orange,
     Red
 
 };
+
+//Main function
+int main()
+{
+    printf("Hello everyone and welcome to this program!\n\n");
+    printf("Enter a integer number between 1 to 3: ");
+    scanf("%d", &User_Number);
+
+    switch(User_Number){
+
+    case Green:
+        printf("\nYour option selected it's Green Traffic Light. ");
+    break;
+
+    case Orange:
+        printf("\nYour option selected it's Orange Traffic Light. ");
+        break;
+
+    case Red:
+        printf("\nYour option selected it's Red Traffic Light. ");
+        break;
+
+    default:
+        printf("\nYour option selected is not valid. ");
+        break;
+
+    }
+
+    return 0;
+}
+
+
